7bca3e19d1c4ea60a561111e9f887fc6d9f00cb1
Lorenzo Tanganelli <56673997+LorenzoTanga-bot@users.noreply.github.com>
2020-09-24 16:27:52 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Arkanoid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.informazioni = new System.Windows.Forms.Label();
+            this.lblPunteggio = new System.Windows.Forms.Label();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.racchetta = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racchetta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.racchetta);
+            this.panel1.Controls.Add(this.informazioni);
+            this.panel1.Controls.Add(this.lblPunteggio);
+            this.panel1.Controls.Add(this.Ball);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1217, 569);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // informazioni
+            // 
+            this.informazioni.AutoSize = true;
+            this.informazioni.Font = new System.Drawing.Font("Ravie", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.informazioni.Location = new System.Drawing.Point(92, 125);
+            this.informazioni.Name = "informazioni";
+            this.informazioni.Size = new System.Drawing.Size(528, 43);
+            this.informazioni.TabIndex = 2;
+            this.informazioni.Text = "Press \'SPACE\' to start";
+            // 
+            // lblPunteggio
+            // 
+            this.lblPunteggio.AutoSize = true;
+            this.lblPunteggio.Font = new System.Drawing.Font("Ravie", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblPunteggio.Location = new System.Drawing.Point(3, 0);
+            this.lblPunteggio.Name = "lblPunteggio";
+            this.lblPunteggio.Size = new System.Drawing.Size(294, 43);
+            this.lblPunteggio.TabIndex = 1;
+            this.lblPunteggio.Text = "lblPunteggio";
+            this.lblPunteggio.Visible = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Red;
+            this.Ball.Location = new System.Drawing.Point(400, 400);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(30, 30);
+            this.Ball.TabIndex = 1;
+            this.Ball.TabStop = false;
+            // 
+            // racchetta
+            // 
+            this.racchetta.BackColor = System.Drawing.Color.Black;
+            this.racchetta.Location = new System.Drawing.Point(32, 539);
+            this.racchetta.Name = "racchetta";
+            this.racchetta.Size = new System.Drawing.Size(200, 20);
+            this.racchetta.TabIndex = 3;
+            this.racchetta.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 494);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racchetta)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Label lblPunteggio;
+        private System.Windows.Forms.Label informazioni;
+        private System.Windows.Forms.PictureBox racchetta;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Arkanoid
+{
+    public partial class Form1 : Form
+    {
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape[] arrayMattoni = new Microsoft.VisualBasic.PowerPacks.RectangleShape[100];
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer contenitoreMattoni;
+
+
+        private int j = 0;
+        private double moviumentoX;
+        private double movimentoY = 5;
+        private Rectangle cursore;
+        private Rectangle[] arrayRettangoli = new Rectangle[100];
+        private int punteggio = 0;
+        private Random _random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            contenitoreMattoni = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+
+            Cursor.Hide();
+
+            FormBorderStyle = FormBorderStyle.None;
+            TopMost = true;
+            Bounds = Screen.PrimaryScreen.Bounds;
+
+            panel1.Size = ClientSize;
+
+            informazioni.Left = (panel1.Width / 2) - (informazioni.Width / 2);
+            informazioni.Top = (panel1.Height / 2) - (informazioni.Height / 2);
+
+            racchetta.Top = panel1.Bottom - (panel1.Bottom / 10);
+
+            Ball.Top = panel1.Height / 2;
+            Ball.Left = panel1.Width / 2;
+
+            lblPunteggio.Location = new Point(15, 15);
+
+            cursore = new Rectangle(Ball.Location.X, Ball.Location.Y, Ball.Width, Ball.Height);
+
+            for(int riga = panel1.Height / 30; riga < panel1.Height / 3; riga += panel1.Height / 20)
+            {
+                for (double colonna = panel1.Width / 100; colonna < panel1.Width; colonna += panel1.Width / 9)
+                {
+                    int larghezzaMattone = Convert.ToInt32(panel1.Width / 10);
+                    int fineRiga = Convert.ToInt32(colonna) + panel1.Width / 10;
+                    if (fineRiga < panel1.Width)
+                    {
+                        arrayMattoni[j] = new Microsoft.VisualBasic.PowerPacks.RectangleShape(Convert.ToInt32(colonna), 70 + riga, larghezzaMattone, 30);
+                        arrayMattoni[j].FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+                        arrayMattoni[j].FillColor = Color.FromArgb(_random.Next(0, 255), _random.Next(0, 255), _random.Next(0, 255));
+                    }
+                    j++;
+                }
+            }
+
+            contenitoreMattoni.Location = new System.Drawing.Point(0, 0);
+            contenitoreMattoni.Margin = new System.Windows.Forms.Padding(0);
+            contenitoreMattoni.Name = "contenitoreMattoni";
+
+            for (int k = j; k > 0; k--)
+            {
+                contenitoreMattoni.Shapes.Add(arrayMattoni[k - 1]);  
+            }
+
+            contenitoreMattoni.Size = new System.Drawing.Size(663, 329);
+            contenitoreMattoni.TabIndex = 13;
+            contenitoreMattoni.TabStop = false;
+
+            panel1.Controls.Add(contenitoreMattoni);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) Close();
+            if (e.KeyCode == Keys.Space)
+            {
+                moviumentoX = _random.Next(1, 11);
+                movimentoY = 5;
+                Ball.Top = panel1.Height / 2;
+                Ball.Left = panel1.Width / 2;
+                punteggio = 0;
+                lblPunteggio.Text = "0";
+                timer1.Enabled = true;
+                informazioni.Visible = false;
+                lblPunteggio.Visible = true;
+                for (int k = j; k > 0; k--)
+                {
+                    contenitoreMattoni.Shapes.Add(arrayMattoni[k - 1]);
+                    arrayMattoni[k - 1].Visible = true;
+                }
+                panel1.Controls.Add(contenitoreMattoni);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            int i = 0;
+
+            for (int riga = panel1.Height / 30; riga < panel1.Height / 3; riga += panel1.Height / 20) 
+            {
+
+                for (double colonna = panel1.Width / 100; colonna < panel1.Width; colonna += panel1.Width / 9) 
+                {
+
+                    int larghezzaMattone = Convert.ToInt32(panel1.Width / 10);
+                    int fineRiga = Convert.ToInt32(colonna) + panel1.Width / 10;
+
+                    if (fineRiga < panel1.Width)
+                    {
+                        arrayRettangoli[i] = new Rectangle(Convert.ToInt32(colonna), 70 + riga, larghezzaMattone, 30);
+                    }
+                    i++;
+                }
+            }
+            Graphics objGraph = e.Graphics;
+            Pen p = new Pen(Brushes.Green);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            racchetta.Left = Cursor.Position.X - (racchetta.Width / 2);
+            cursore.Location = Ball.Location;
+
+            Ball.Left += Convert.ToInt32(moviumentoX);
+            Ball.Top += Convert.ToInt32(movimentoY);
+
+            if (Ball.Bottom >= racchetta.Top && Ball.Bottom <= racchetta.Bottom && Ball.Left >= racchetta.Left && Ball.Right <= racchetta.Right)
+            {
+                movimentoY += 0.5;
+                moviumentoX += 0.5;
+                movimentoY = -movimentoY;
+                lblPunteggio.Text = punteggio.ToString();
+            }
+
+            if (Ball.Left <= panel1.Left)
+            {
+                moviumentoX = -moviumentoX;
+            }
+
+            if (Ball.Right >= panel1.Right)
+            {
+                moviumentoX = -moviumentoX;
+            }
+
+            if (Ball.Top <= panel1.Top)
+            {
+                movimentoY = -movimentoY;
+            }
+
+            if (Ball.Bottom >= panel1.Bottom)
+            {
+                timer1.Enabled = false;
+                informazioni.Text += "\n Score " + punteggio;
+                informazioni.Visible = true;
+                lblPunteggio.Visible = false;
+            }
+            for (int l = j; l > 0; l--)
+            {
+                if (arrayMattoni[l - 1].Visible == true && arrayRettangoli[l - 1].IntersectsWith(cursore))
+                {
+                    arrayMattoni[l - 1].Visible = false;
+                    punteggio += 1;
+                    movimentoY = -movimentoY;
+                    lblPunteggio.Text = punteggio.ToString();
+                }
+            }
+            if(punteggio == 54)
+            {
+                timer1.Enabled = false;
+                informazioni.Text += "\n Score " + punteggio;
+                informazioni.Visible = true;
+                lblPunteggio.Visible = false;
+            }
+        }
+    }
+}
